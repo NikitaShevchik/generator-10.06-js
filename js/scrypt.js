ab7c5ff9efc7464cf60a9889df95b65328da5e27
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-10 16:23:48 +0300

    generator + iterator + symbol

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,12 +1,57 @@
 "use strict"
 
-function* strGenerator() {
-    yield 'H'
-    yield 'E'
-    yield 'L'
-    yield 'L'
-    yield 'O'
+// function* strGenerator() {
+//     yield 'H'
+//     yield 'E'
+//     yield 'L'
+//     yield 'L'
+//     yield 'O'
+// }
+
+// const str = strGenerator()
+
+// function* numberGen(n = 10) {
+//     for (let i=0; i < n; i++) {
+//         yield i
+//     }
+// }
+
+// const count = numberGen(11)
+
+
+// const iterator = {
+//     [Symbol.iterator](n = 10) {
+//         let i = 0
+
+//         return {
+//             next() {
+//                 if (i < n) {
+//                     return {
+//                         value: ++i,
+//                         done: false
+//                     }
+//                 } return { value: undefined, done: true }
+//             }
+//         }
+//     }
+// }
+
+
+// for (let k of 'Hello') {
+//     console.log(k)
+// }
+
+// for (let k of [1, 1, 2, 3, 5, 8, 13]) {
+//     console.log(k)
+// }
+
+
+function* iter(n = 10) {
+    for (let i = 0; i < n; i++) {
+        yield i
+    }
 }
 
-const str = strGenerator()
-
+for (let k of iter(6)) {
+    console.log(k)
+}
