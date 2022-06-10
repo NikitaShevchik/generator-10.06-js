b609f30bf35807bc4c4dd53fa83b9ecb2872abb2
NikitaShevchik <shevchik.nikita@gmail.com>
2022-06-10 16:59:25 +0300

    for+forEach+Map+Filter

--- js/scrypt.js
+++ js/scrypt.js
@@ -46,12 +46,68 @@
 // }
 
 
-function* iter(n = 10) {
-    for (let i = 0; i < n; i++) {
-        yield i
-    }
-}
+// function* iter(n = 10) {
+//     for (let i = 0; i < n; i++) {
+//         yield i
+//     }
+// }
 
-for (let k of iter(6)) {
-    console.log(k)
-}
+// for (let k of iter(6)) {
+//     console.log(k)
+// }
+
+/*---------МЕТОДЫ МАССИВОВ----------*/
+
+
+
+const people = [
+    { name: 'Никита', age: 22, budget: 28000 },
+    { name: 'Иван', age: 30, budget: 150000 },
+    { name: 'Владислав', age: 15, budget: 7000 },
+    { name: 'Александр', age: 54, budget: 1500 },
+    { name: 'Владимир', age: 17, budget: 16800 },
+    { name: 'Олег', age: 42, budget: 7600 },
+]
+
+// for (let i = 0; i < people.length; i++){
+//     console.log(people[i])
+// }
+
+// for (let person of people) {
+//     console.log(person)
+// }
+
+//HIGHER ORDER FUNCTIONS
+
+// ForEach
+// people.forEach(function (person) {
+//     console.log(person)
+// })
+
+// people.forEach(person => console.log(person))
+
+// Map
+// const newPeople = people.map(person => (person.age * 1.1).toFixed(0))
+// console.log(newPeople)
+
+
+// Filter
+
+// const alc = []
+// for (let i = 0; i < people.length; i++) {
+//     if (people[i].age >= 18) {
+//         alc.push(people[i])
+//     }
+// }
+// console.log(alc)
+// const alc = people.filter(person => person.age >= 18)
+// console.log(alc)
+
+
+// Reduce
+
+
+
+// Find
+
+// FindIndex
